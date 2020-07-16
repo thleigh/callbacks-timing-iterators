7a94f56e5a2a785aaf7322eeec9fa808a5bd31d6
Tanner Leigh <tannerhleigh@gmail.com>
2020-07-16 15:28:06 -0700

    [callbacks] trying to attempt a callback excercise

--- app.js
+++ app.js
@@ -15,3 +15,42 @@ function anotherGreeting() {
 
 let myName = "Tanner";
 console.log(greeting(myName, anotherGreeting));
+
+//Exericise
+function printArray(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i];
+
+        console.log(number);
+    }
+    callback(array);
+}
+
+function printArrayAgain(array) {
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i];
+
+        console.log(number);
+    }
+}
+
+const numbersArray = [1,2,3,4,5,9,8,7,6];
+
+printArray(numbersArray, printArrayAgain);
+
+//Exercise
+function calculator(num1, num2, callbackOne, callbackTwo) {
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+
+    return result;
+}
+
+function addNumbers(num1, num2) {
+    let result = num1 + num2;
+}
+
+function subtractNumbers(num1, num2) {
+    let result = num1 + num2;
+}
+
+console.log(calculator(5,77, addNumbers, subtractNumbers));
